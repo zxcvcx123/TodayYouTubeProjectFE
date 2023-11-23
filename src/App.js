ae513b8118ff8ae41a1cc788d1cbcaa04de92e58
JungMo <jmjm0611@naver.com>
2023-11-23 17:31:56 +0900

    routes 설정 (정정모)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { HomeLayout } from "./layout/Homelayout";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout />} />,
+    // <Route path="경로명" element={컴포넌트} />
+  ),
+);
+
 function App() {
-  return <div>Start React!!</div>;
+  // 건들지 마시오
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
