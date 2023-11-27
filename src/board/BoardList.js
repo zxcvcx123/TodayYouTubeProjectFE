52d804d811d9b799fcc945fca5c11e7019e6afd3
Nighteast <jung946244@naver.com>
2023-11-27 16:34:00 +0900

    seongwon 게시글 목록 전체 wrap 처리

--- src/board/BoardList.js
+++ src/board/BoardList.js
@@ -33,91 +33,95 @@ function BoardList() {
   }, []);
 
   return (
-    <Box>
-      {/* 게시글 목록 상단 바 */}
-      <Flex justifyContent={"space-between"}>
+    <Flex justifyContent={"center"}>
+      <Box w={"90%"}>
         <Box>
-          <Button onClick={() => navigate("/write")} colorScheme="blue">
-            글쓰기
-          </Button>
-        </Box>
-        <Box>
-          <Tooltip label={"리스트 형태 보기"}>
-            <Button>
-              <FontAwesomeIcon icon={faList} />
-            </Button>
-          </Tooltip>
-          <Tooltip label={"격자 형태 보기 "}>
-            <Button>
-              <FontAwesomeIcon icon={faBorderAll} />
-            </Button>
-          </Tooltip>
-        </Box>
-      </Flex>
+          {/* 게시글 목록 상단 바 */}
+          <Flex justifyContent={"space-between"}>
+            <Box>
+              <Button onClick={() => navigate("/write")} colorScheme="blue">
+                글쓰기
+              </Button>
+            </Box>
+            <Box>
+              <Tooltip label={"리스트 형태 보기"}>
+                <Button>
+                  <FontAwesomeIcon icon={faList} />
+                </Button>
+              </Tooltip>
+              <Tooltip label={"격자 형태 보기 "}>
+                <Button>
+                  <FontAwesomeIcon icon={faBorderAll} />
+                </Button>
+              </Tooltip>
+            </Box>
+          </Flex>
 
-      <Table size={"sm"}>
-        <Thead>
-          <Tr>
-            <Th textAlign={"center"}>번호</Th>
-            <Th textAlign={"center"}>제목</Th>
-            <Th textAlign={"center"}>좋아요</Th>
-            <Th textAlign={"center"}>작성자</Th>
-            <Th textAlign={"center"}>작성일시</Th>
-            <Th textAlign={"center"}>조회수</Th>
-          </Tr>
-        </Thead>
-
-        <Tbody>
-          {boardList &&
-            boardList.map((board) => (
-              <Tr
-                key={board.id}
-                onClick={() => navigate("/board/" + board.id)}
-                _hover={{ backgroundColor: "lightcyan", cursor: "pointer" }}
-              >
-                {/* 삭제된 게시물일 경우 표기가 변동 */}
-                {board.is_show ? (
-                  <>
-                    <Td textAlign={"center"}>{board.id}</Td>
-                    <Td>
-                      <Flex align={"center"} gap={"10px"}>
-                        <YoutubeInfo
-                          link={board.link}
-                          extraThumbnail={true}
-                          thumbnailWidth={120}
-                          thumbnailHeight={70}
-                          toolTip={true}
-                        />
-
-                        {/* 길이가 길 경우 20자로 제한하고 나머지는 ...으로 표시 */}
-                        {/* 짤린 제목에 커서를 올릴 시 제목이 툴팁으로 나타남 */}
-                        {board.title.length > 20 ? (
-                          <Tooltip label={board.title}>
-                            <Text>{`${board.title.slice(0, 20)}...`}</Text>
-                          </Tooltip>
-                        ) : (
-                          <Text>{board.title}</Text>
-                        )}
-                      </Flex>
-                    </Td>
-                    <Td textAlign={"center"}>좋아요</Td>
-                    <Td textAlign={"center"}>{board.board_member_id}</Td>
-                    <Td textAlign={"center"}>{board.created_at}</Td>
-                    <Td textAlign={"center"}>조회수</Td>
-                  </>
-                ) : (
-                  <>
-                    <Td textAlign={"center"}>{board.id}</Td>
-                    <Td colSpan={5}>
-                      <Text textAlign={"center"}>삭제된 게시물입니다.</Text>
-                    </Td>
-                  </>
-                )}
+          <Table size={"sm"}>
+            <Thead>
+              <Tr>
+                <Th textAlign={"center"}>번호</Th>
+                <Th textAlign={"center"}>제목</Th>
+                <Th textAlign={"center"}>좋아요</Th>
+                <Th textAlign={"center"}>작성자</Th>
+                <Th textAlign={"center"}>작성일시</Th>
+                <Th textAlign={"center"}>조회수</Th>
               </Tr>
-            ))}
-        </Tbody>
-      </Table>
-    </Box>
+            </Thead>
+
+            <Tbody>
+              {boardList &&
+                boardList.map((board) => (
+                  <Tr
+                    key={board.id}
+                    onClick={() => navigate("/board/" + board.id)}
+                    _hover={{ backgroundColor: "lightcyan", cursor: "pointer" }}
+                  >
+                    {/* 삭제된 게시물일 경우 표기가 변동 */}
+                    {board.is_show ? (
+                      <>
+                        <Td textAlign={"center"}>{board.id}</Td>
+                        <Td>
+                          <Flex align={"center"} gap={"10px"}>
+                            <YoutubeInfo
+                              link={board.link}
+                              extraThumbnail={true}
+                              thumbnailWidth={120}
+                              thumbnailHeight={70}
+                              toolTip={true}
+                            />
+
+                            {/* 길이가 길 경우 20자로 제한하고 나머지는 ...으로 표시 */}
+                            {/* 짤린 제목에 커서를 올릴 시 제목이 툴팁으로 나타남 */}
+                            {board.title.length > 20 ? (
+                              <Tooltip label={board.title}>
+                                <Text>{`${board.title.slice(0, 20)}...`}</Text>
+                              </Tooltip>
+                            ) : (
+                              <Text>{board.title}</Text>
+                            )}
+                          </Flex>
+                        </Td>
+                        <Td textAlign={"center"}>좋아요</Td>
+                        <Td textAlign={"center"}>{board.board_member_id}</Td>
+                        <Td textAlign={"center"}>{board.created_at}</Td>
+                        <Td textAlign={"center"}>조회수</Td>
+                      </>
+                    ) : (
+                      <>
+                        <Td textAlign={"center"}>{board.id}</Td>
+                        <Td colSpan={5}>
+                          <Text textAlign={"center"}>삭제된 게시물입니다.</Text>
+                        </Td>
+                      </>
+                    )}
+                  </Tr>
+                ))}
+            </Tbody>
+          </Table>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
 
