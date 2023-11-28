3d117872778d2c5bc5156d0af25e2c6cb30cd455
junggji <calrinar@gmail.com>
2023-11-28 18:45:07 +0900

    게시물 페이징 기능 추가, 게시물 내 검색기능 추가(지환)

--- src/page/Pagination.js
+++ src/page/Pagination.js
@@ -1,24 +1,82 @@
 import React, { useState } from "react";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Center, Spinner } from "@chakra-ui/react";
 import axios from "axios";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleLeft,
+  faAngleRight,
+  faAnglesLeft,
+  faAnglesRight,
+} from "@fortawesome/free-solid-svg-icons";
 
-function Pagination(props) {
-  const pageNumbers = [];
+function PageButton({ variant, children, pageNumber }) {
+  const navigate = useNavigate();
+  const [params] = useSearchParams();
 
-  function handlePage() {
-    axios.get("/api/paging/");
+  function handleClick() {
+    params.set("p", pageNumber);
+    navigate("?" + params);
   }
 
   return (
-    <Box>
-      <Button onClick={handlePage} value={1}>
-        1
-      </Button>
-      <Button>2</Button>
-      <Button>3</Button>
-      <Button>4</Button>
-      <Button>5</Button>
-    </Box>
+    <Button variant={variant} onClick={handleClick}>
+      {children}
+    </Button>
+  );
+}
+
+function Pagination({ pageInfo }) {
+  const navigate = useNavigate();
+  const pageNumbers = [];
+
+  // if (pageInfo == null) {
+  //   return <Spinner />;
+  // }
+
+  for (let i = pageInfo.startPageNumber; i <= pageInfo.endPageNumber; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <Center>
+      <Box>
+        {pageInfo.initialPage && (
+          <PageButton variant="ghost" pageNumber={pageInfo.initialPage}>
+            <FontAwesomeIcon icon={faAnglesLeft} />
+          </PageButton>
+        )}
+        {pageInfo.prevPageNumber && (
+          <PageButton variant="ghost" pageNumber={pageInfo.prevPageNumber}>
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </PageButton>
+        )}
+
+        {pageNumbers.map((pageNumber) => (
+          <PageButton
+            key={pageNumber}
+            variant={
+              pageNumber === pageInfo.currentPageNumber ? "solid" : "ghost"
+            }
+            pageNumber={pageNumber}
+          >
+            {pageNumber}
+          </PageButton>
+        ))}
+
+        {pageInfo.nextPageNumber && (
+          <PageButton variant="ghost" pageNumber={pageInfo.nextPageNumber}>
+            <FontAwesomeIcon icon={faAngleRight} />
+          </PageButton>
+        )}
+
+        {pageInfo.lastPageNumber && (
+          <PageButton variant="ghost" pageNumber={pageInfo.lastPageNumber}>
+            <FontAwesomeIcon icon={faAnglesRight} />
+          </PageButton>
+        )}
+      </Box>
+    </Center>
   );
 }
 
