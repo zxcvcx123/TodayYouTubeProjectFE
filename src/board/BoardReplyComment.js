c3a99abb1837b7ad8368c534da32b4d0637e532a
kwangsik Kim <penggusic@gmail.com>
2023-11-28 12:41:54 +0900

    대댓글 crud 작업 중(c,r까지 완료)(광식)

--- src/board/BoardReplyComment.js
+++ src/board/BoardReplyComment.js
@@ -1,17 +1,27 @@
-import { Box, Button, Input, Text, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+  StackDivider,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
 import { SmallAddIcon } from "@chakra-ui/icons";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import YouTube from "react-youtube";
 
-function ReplyCommentForm({ comment_id }) {
+function ReplyCommentForm({ comment_id, isSubmitting, onSubmit }) {
   const [reply_comment, setReply_comment] = useState("");
 
   function handleReplySubmit() {
-    axios.post("/api/comment/reply/add", {
-      comment_id: comment_id,
-      reply_comment: reply_comment,
-    });
+    onSubmit({ comment_id, reply_comment });
   }
 
   return (
@@ -20,20 +30,72 @@ function ReplyCommentForm({ comment_id }) {
         value={reply_comment}
         onChange={(e) => setReply_comment(e.target.value)}
       />
-      <Button onClick={handleReplySubmit}>쓰기</Button>
+      <Button isDisabled={isSubmitting} onClick={handleReplySubmit}>
+        쓰기
+      </Button>
     </Box>
   );
 }
 
-function ReplyCommentList() {
-  return <Box>대댓리스트</Box>;
+function ReplyCommentList({ reply_commentList }) {
+  return (
+    <Card>
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing={4}>
+          {reply_commentList.map((reply_comment) => (
+            <Box key={reply_comment.id}>
+              <Flex justifyContent="space-between">
+                <Heading size="xs" bg="whitesmoke" borderRadius="5">
+                  {reply_comment.nickname}({reply_comment.member_id})
+                </Heading>
+                <Text fontSize="xs">{reply_comment.created_at}</Text>
+              </Flex>
+              <Text sx={{ whiteSpace: "pre-wrap" }} pt={2} fontSize="sm">
+                {reply_comment.reply_comment}
+              </Text>
+            </Box>
+          ))}
+        </Stack>
+      </CardBody>
+    </Card>
+  );
 }
 
 export function BoardReplyComment({ comment_id }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const [reply_commentList, setReply_commentList] = useState([]);
+
+  useEffect(() => {
+    if (!isSubmitting) {
+      const params = new URLSearchParams();
+      params.set("reply_id", comment_id);
+
+      axios
+        .get("/api/comment/reply/list?" + params)
+        .then((response) => setReply_commentList(response.data));
+    }
+  }, [isSubmitting]);
+
+  function handleReplySubmit(reply_comment) {
+    setIsSubmitting(true);
+
+    axios
+      .post("/api/comment/reply/add", reply_comment)
+      .finally(() => setIsSubmitting(false));
+  }
+
   return (
     <Box>
-      <ReplyCommentForm comment_id={comment_id} />
-      <ReplyCommentList />
+      <ReplyCommentForm
+        comment_id={comment_id}
+        isSubmitting={isSubmitting}
+        onSubmit={handleReplySubmit}
+      />
+      <ReplyCommentList
+        comment_id={comment_id}
+        reply_commentList={reply_commentList}
+      />
     </Box>
   );
 }
