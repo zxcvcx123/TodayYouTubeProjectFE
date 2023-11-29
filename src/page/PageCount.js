d8d42ad430dd98c8cfbbf2c69bcce865fbc1f2a9
junggji <calrinar@gmail.com>
2023-11-29 15:44:00 +0900

    게시물 5,10,20개씩 보기 완성(지환)

--- src/page/PageCount.js
+++ src/page/PageCount.js
@@ -22,48 +22,25 @@ function PageCount(props) {
     navigate("?" + params);
   }, [pageCount]);
 
-  function handleCountClick(e) {
-    setPageCount(e.target.value);
-  }
-
   return (
-    <Select
-      border={"1px"}
-      borderColor={"gray.200"}
-      padding={1}
-      width={"20%"}
-      size={"sm"}
-      defaultValue={10}
-      onChange={(e) => handleCountClick(e)}
-    >
-      <option fontSize={"0.9rem"} mr={5} ml={3} value={5}>
-        5개씩 보기
-      </option>
-      <option fontSize={"0.9rem"} mr={5} ml={3} value={10}>
-        10개씩 보기
-      </option>
-      <option fontSize={"0.9rem"} mr={5} ml={3} value={20}>
-        20개씩 보기
-      </option>
-    </Select>
-
     // radio로 해본것
-    // <Box>
-    //   <RadioGroup
-    //     border="1px"
-    //     borderColor="gray.200"
-    //     padding={1}
-    //     size={"sm"}
-    //     value={pageCount}
-    //     onChange={setPageCount}
-    //   >
-    //     <Stack spacing={4} direction="row">
-    //       <Radio value="5">5개씩 보기</Radio>
-    //       <Radio value="10">10개씩 보기</Radio>
-    //       <Radio value="20">20개씩 보기</Radio>
-    //     </Stack>
-    //   </RadioGroup>
-    // </Box>
+
+    <Box>
+      <RadioGroup
+        border="1px"
+        borderColor="gray.200"
+        padding={1}
+        size={"sm"}
+        value={pageCount}
+        onChange={setPageCount}
+      >
+        <Stack spacing={4} direction="row">
+          <Radio value="5">5개씩 보기</Radio>
+          <Radio value="10">10개씩 보기</Radio>
+          <Radio value="20">20개씩 보기</Radio>
+        </Stack>
+      </RadioGroup>
+    </Box>
 
     // <Flex border={"1px"} borderColor={"gray.200"} padding={1}>
     //   <Box
