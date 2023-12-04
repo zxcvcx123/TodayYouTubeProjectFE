2c0857c992c0f200b74605d225bd1b015b2979bd
junggji <calrinar@gmail.com>
2023-12-04 19:02:43 +0900

    메인게시판 랭킹순 가져온 후에 출력완료, Youtube 컴포넌트 크기관련 추가(지환)

--- src/layout/MainView.js
+++ src/layout/MainView.js
@@ -3,6 +3,8 @@ import {
   Button,
   Center,
   Flex,
+  Text,
+  FormHelperText,
   IconButton,
   Menu,
   MenuButton,
@@ -13,13 +15,16 @@ import {
   MenuOptionGroup,
   Select,
   Spinner,
+  StatHelpText,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { MainBoardList } from "./MainBoardList";
 import axios from "axios";
-import { AddIcon, HamburgerIcon } from "@chakra-ui/icons";
+import { AddIcon, ChevronDownIcon, HamburgerIcon } from "@chakra-ui/icons";
 import { useLocation, useNavigate } from "react-router-dom";
 import YoutubeInfo from "../component/YoutubeInfo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRankingStar } from "@fortawesome/free-solid-svg-icons";
 
 export function MainView() {
   const [category, setCategory] = useState("all");
@@ -41,7 +46,6 @@ export function MainView() {
 
   function handleCategoryChange(e) {
     setCategory(e.target.value);
-    console.log(category);
   }
 
   if (firstList == null) {
@@ -57,34 +61,30 @@ export function MainView() {
   // 임시메인
   return (
     <Box bg="black" w="100%" h="700px" p={4}>
-      {/* menulist 하던거 */}
+      {/*menulist 하던거*/}
       {/*<Menu>*/}
-      {/*  <MenuButton*/}
-      {/*    onChange={(e) => handleCategoryChange(e)}*/}
-      {/*    as={IconButton}*/}
-      {/*    aria-label="Options"*/}
-      {/*    icon={<HamburgerIcon />}*/}
-      {/*  />*/}
-      {/*  <MenuList>*/}
-      {/*    <MenuOptionGroup defaultValue="all" value={category} title="게시판">*/}
+      {/*  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>*/}
+      {/*    게시판 선택*/}
+      {/*  </MenuButton>*/}
+      {/*  <MenuList value={category} onChange={(e) => handleCategoryChange(e)}>*/}
+      {/*    <MenuOptionGroup title="게시판">*/}
       {/*      <MenuItemOption value="all">전체</MenuItemOption>*/}
-      {/*      <MenuItemOption value="mukbang">먹방</MenuItemOption>*/}
-      {/*      <MenuItemOption value="game">겜</MenuItemOption>*/}
-      {/*      <MenuItemOption value="4">4</MenuItemOption>*/}
-      {/*      <MenuItemOption value="5">5</MenuItemOption>*/}
+      {/*      <MenuItemOption value="C003">먹방</MenuItemOption>*/}
+      {/*      <MenuItemOption value="C007">겜</MenuItemOption>*/}
+      {/*      <MenuItemOption value="C004">일상</MenuItemOption>*/}
+      {/*      <MenuItemOption value="C002">스포츠</MenuItemOption>*/}
       {/*    </MenuOptionGroup>*/}
       {/*  </MenuList>*/}
       {/*</Menu>*/}
-
       <Select
         onChange={handleCategoryChange}
         pos="absolute"
-        top="40"
+        top="60"
         left="10"
         width="15%"
-        border={"1px"}
-        borderColor={"red"}
         backgroundColor="white"
+        size="sm"
+        alignItems="center"
       >
         <option value="all">전체게시판</option>
         <option value="C002">스포츠게시판</option>
@@ -94,29 +94,33 @@ export function MainView() {
         <option value="C006">영화/드라마 게시판</option>
         <option value="C007">게임게시판</option>
       </Select>
-      <Center w="60%" h="70%" m="auto" border={"1px"} borderColor={"red"}>
-        <Box width={"100%"} height="100%">
-          {firstList &&
-            firstList.map((first) => (
+      <Box color="white" ml={10}>
+        <FontAwesomeIcon icon={faRankingStar} /> 주간 베스트 영상
+        <Text fontSize="0.8rem" color="gray.400">
+          - 한 주간 가장 추천을 많이 받는 영상들입니다.
+        </Text>
+      </Box>
+      <Center w="60%" h="70%" m="auto" mt={"-40px"}>
+        {firstList &&
+          firstList.map((first) => (
+            <Box width={"100%"} height="100%" key={first.id}>
               <YoutubeInfo
                 link={first.link}
                 extraVideo={true}
-                thumbnailWidth={320}
-                thumbnailHeight={180}
+                opts={{ height: "470", width: "1050" }}
               />
-            ))}
-        </Box>
+            </Box>
+          ))}
       </Center>
       <Center mt={5}>
         <Flex w="70%" h="100%" m="auto" mt={2} justify="space-between">
           {otherList &&
             otherList.map((other) => (
-              <Box w="22%" h={"50%"} border="1px" borderColor="blue">
+              <Box w="22%" h={"50%"} key={other.id}>
                 <YoutubeInfo
                   link={other.link}
                   extraVideo={true}
-                  thumbnailWidth={50}
-                  thumbnailHeight={30}
+                  opts={{ height: 180, width: 250 }}
                 />
               </Box>
             ))}
