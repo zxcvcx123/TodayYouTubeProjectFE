f8fc6fbd885326600a7df174c8a14009ecca0afa
Nighteast <jung946244@naver.com>
2023-12-07 14:37:36 +0900

    seongwon 게시물 작성시 name_eng: boardInfo로 게시판 카테고리 영어명 보내주기

--- src/board/BoardWrite.js
+++ src/board/BoardWrite.js
@@ -112,6 +112,7 @@ function BoardWrite() {
         uploadFiles,
         uuSrc,
         board_member_id: loginInfo.member_id,
+        name_eng: boardInfo,
       })
       .then(() => {
         toast({
