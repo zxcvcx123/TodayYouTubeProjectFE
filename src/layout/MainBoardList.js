e2b78a0f81992d6e057565ea63249d2cfbe76995
kwangsik Kim <penggusic@gmail.com>
2023-12-07 20:34:13 +0900

    메인 서브게시판 완료(광식)

--- src/layout/MainBoardList.js
+++ src/layout/MainBoardList.js
@@ -41,8 +41,6 @@ export function MainBoardList({
     navigate(`/board/${postId}`);
   };
 
-
-
   const renderListItem = (board, index) => (
     <ListItem
       _hover={{ bg: "gray.100", cursor: "pointer" }}
