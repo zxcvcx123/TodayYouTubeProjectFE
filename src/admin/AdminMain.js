920c34331e0c0a11435d36efdf07009c630fed95
Nighteast <jung946244@naver.com>
2023-12-10 00:22:46 +0900

    seongwon BarChart 기본 세팅 동작 테스트

--- src/admin/AdminMain.js
+++ src/admin/AdminMain.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Sidenav} from "./Sidenav";
 import {Box, Flex} from "@chakra-ui/react";
 import {BarChart} from "./BarChart";
+import axios from "axios";
 
 function AdminMain() {
+  const [boardData, setBoardData] = useState({
+    labels: [1,2,3,4],
+    datasets:[{
+      label: "board",
+      data: [20,10,30,50]
+    }]
+  });
+
+  // useEffect(() => {
+  //   axios.get("/api/admin/board")
+  //     .then(response => console.log(response.data));
+  // }, []);
+
+
   return (
     <Flex>
       {/* ---------- 사이드 바 ----------*/}
       <Sidenav/>
       {/* ---------- 메인 ----------*/}
       <Box>
-        <BarChart chartData={}/>
+        <BarChart chartData={boardData}/>
       </Box>
     </Flex>
   );
--- src/admin/BarChart.js
+++ src/admin/BarChart.js
@@ -1,5 +1,6 @@
 import {Bar} from "react-chartjs-2";
-import React from "react";
+import React, {useState} from "react";
+import { Chart as ChartJS } from "chart.js/auto"
 
 export function BarChart({chartData}) {
   return <Bar data={chartData}/>;
