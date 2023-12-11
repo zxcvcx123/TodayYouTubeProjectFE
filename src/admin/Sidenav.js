e5b0ba8c43e8ea77a3b91fc0f99c77bc1fb2082b
Nighteast <jung946244@naver.com>
2023-12-11 18:56:42 +0900

    관리자 메뉴 버튼 임시 생성

--- src/admin/Sidenav.js
+++ src/admin/Sidenav.js
@@ -6,8 +6,8 @@ export function Sidenav() {
     <VStack bg={"black"} color={"white"} w={40} h={500} border={"1px solid red"}>
       <Heading as='h4' size='md'>관리자 페이지</Heading>
       <Button>메인</Button>
-      <Button>ㅋ</Button>
-      <Button>ㅋ</Button>
+      <Button>페이지1</Button>
+      <Button>페이지2</Button>
     </VStack>
   );
 }
