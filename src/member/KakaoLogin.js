f88e50d5b8e1666b6992b42042293e2622b21c59
Nighteast <jung946244@naver.com>
2023-12-14 20:19:53 +0900

    카카오 로그인 컴포넌트 추가

--- src/member/KakaoLogin.js
+++ src/member/KakaoLogin.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Image} from "@chakra-ui/react";
 
 function KakaoLogin() {
   const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
   const REDIRECT_URI = 'http://localhost:3000/member/login';
-
   const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const code = new URL(window.location.href).searchParams.get('code')
 
   function handleLoginClick() {
     window.location.href = KAKAO_AUTH_URI;
