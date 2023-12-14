7541e882184ff477dfc0e75e2292c937c3f39c6a
Nighteast <jung946244@naver.com>
2023-12-14 21:18:03 +0900

    카카오 로그인 코드 보내기

--- src/member/KakaoLogin.js
+++ src/member/KakaoLogin.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import {Image} from "@chakra-ui/react";
+import axios from "axios";
 
 function KakaoLogin() {
   const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
@@ -7,9 +8,14 @@ function KakaoLogin() {
   const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   const code = new URL(window.location.href).searchParams.get('code')
 
+
+
   function handleLoginClick() {
     window.location.href = KAKAO_AUTH_URI;
+
   }
+    axios.get(`/api/kakao/oauth/token?code=${code}`);
+
 
   return (
     <button onClick={handleLoginClick}>
